9bb653bd9fb374f19d11cc715b9b0bb954b5ff8a
Euan Goddard <euangoddard@manufacture2030.com>
2025-06-09 06:32:41 +0100

    feat: add icons

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -12,19 +12,38 @@ export const RouterHead = component$(() => {
   return (
     <>
       <title>{head.title}</title>
-
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="doc2cal" />
+      <meta property="og:title" content={head.title} />
+      <meta
+        property="og:description"
+        content={head.meta.find((m) => m.name === "description")?.content || ""}
+      />
+      <meta property="og:url" content={loc.url.href} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/logo.png" />
+      <meta property="og:image:alt" content="doc2cal logo" />
+      <meta property="og:site_name" content="doc2cal" />
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
-
       {head.links.map((l) => (
         <link key={l.key} {...l} />
       ))}
-
       {head.styles.map((s) => (
         <style
           key={s.key}
@@ -34,7 +53,6 @@ export const RouterHead = component$(() => {
             : { dangerouslySetInnerHTML: s.style })}
         />
       ))}
-
       {head.scripts.map((s) => (
         <script
           key={s.key}
